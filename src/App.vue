5d356b04925ce984a3f5cd3de2aff023cbd86b2e
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-05-17 13:46:42 +0200

    create menu

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,24 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div>
-      
+      <div class="flex overflow-hidden fixed bottom-0 w-full justify-around items-center py-4 bg-gray">
+        <RouterLink to="/">
+          <img src="./assets/Home.svg" alt="Home">
+        </RouterLink>
+        <RouterLink to="/">
+          <img src="./assets/Mail.svg" alt="Mail">
+        </RouterLink>
+        <RouterLink to="/">
+          <img src="./assets/Plus.svg" alt="Plus">
+        </RouterLink>
+        <RouterLink to="/">
+          <img src="./assets/Heart.svg" alt="Heart">
+        </RouterLink>
+        <RouterLink to="/">
+          <img src="./assets/user.svg" alt="User">
+        </RouterLink>
+      </div>
+    <RouterView/>
   </div>
 </template>
 
