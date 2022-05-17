a84ddd77ecec2a776d1fed21c1d33ae4555980eb
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-05-17 13:46:17 +0200

    Upload Files

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+        name: "",
+        components: {
+            
+        },
+    };
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'gray': '#F9F9F9',
+      'orange': '#eab270',
+      'heartred':'#b91c1c',
+      'dark-gray':'#aeaeae'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
