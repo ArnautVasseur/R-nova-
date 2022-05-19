f15ffc45cb2de1316cb8688827a404e73eb5376c
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-05-19 22:51:25 +0200

    Adding Components

--- src/components/SendButton.vue
+++ src/components/SendButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-40 h-10 bg-bright_green rounded-lg shadow-xl">
+    <button class="w-40 h-10 bg-bright_green rounded-lg shadow-xl">
         <p class="text-center text-white font-bold py-2">Envoyer</p>
-    </div>
+    </button>
 </template>
