d0e25dd8214173c4858fa8a61abc1cc1bf98ff4c
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-06-07 18:42:17 +0200

    images not displaying netlify fixing attempt

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(320px,320px))] justify-around mb-5">
             <a href="achat">
                 <Card class="mb-5"
-                image="public\images\velo-wall-white.png"
+                image="public/images/velo-wall-white.png"
                 :distance="27"
                 state="Neuf"
                 brand="SuperVelo"
